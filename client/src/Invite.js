c6744efb2815d89ed8ce6bb8078f37ba4e8adce3
wip-abramson <wip.abramson@gmail.com>
2020-09-22 14:10:27 +0100

    add qr img

--- client/src/Invite.js
+++ client/src/Invite.js
@@ -2,15 +2,21 @@ import React from 'react'
 import {createInvite} from "./api/connections";
 
 const Invite = () => {
-
+    let [base64qr, setBase64qr] = React.useState(null)
     React.useEffect(() => {
         createInvite().then(response => {
-            console.log(response)
-
-        })
+            console.log(response.data)
+            // console.log(btoa(response.data))
+            // console.log(atob(response.data))
+            setBase64qr(response.data)
+        }, [])
     })
 
-    return (
+    return base64qr ? (
+        <div className="qr-code">
+            <img src={"data:image/png;base64, " + base64qr} />
+        </div>
+    ) : (
         <div>This is an invite</div>
     )
 }
