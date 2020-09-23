8dd6ca0555df89eb0f3680765c6df469b7f9866d
wip-abramson <wip.abramson@gmail.com>
2020-09-23 17:45:27 +0100

    update route

--- server/host_employer/routes.py
+++ server/host_employer/routes.py
@@ -3,4 +3,4 @@ from views import index, invite, check_active
 def setup_routes(app):
     app.router.add_get('/', index)
     app.router.add_get('/invite', invite)
-    app.router.add_get('/connection/{id}/active', check_active)
+    app.router.add_get('/connection/{conn_id}/active', check_active)
